5569f1fa2d9cb5d098a75f7761ee3c39dc7a2cd6
Sam Louloudis <samanthaleelouloudis@gmail.com>
2026-01-12 13:28:43 +0000

    returning from functions

--- demos/week02/02-functions-intro/app.js
+++ demos/week02/02-functions-intro/app.js
@@ -52,3 +52,58 @@ function sum(a, b) {
 // when we invoke our function, we can tell it what a and b should be
 sum(5, 10)
 sum(34234234, -123123123)
+
+
+
+
+
+//// ---------- RETURNING
+
+
+
+// a function called multiply
+// a function declaration
+
+// return has to be inside a function declaration, and it must be the last thing inside that function declartion. 
+function figuresOutCCScore(a, b) {
+    const myFigure = 1300
+
+    return a * b * myFigure
+}
+
+
+
+
+// I'll invoke the multiply function and give it arguments
+const userCCStore = figuresOutCCScore(10 , 5)
+
+console.log(userCCStore)
+
+
+// variables and values that we make inside a function are only availbe inside that function. 
+// console.log(myFigure)
+
+
+function One() {
+    return 1
+}
+
+function Two() {
+    return 2
+}
+
+
+console.log(One() + Two())
+
+
+
+
+
+
+
+
+// turnign a string into a number
+
+// const userNumber = prompt('Enter a humber')
+
+// console.log(Number(userNumber) + 3)
